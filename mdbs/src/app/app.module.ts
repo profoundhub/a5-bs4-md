12c9352e05418a6ceb62ba6ce9beff8c9c232afd
Daniel Lim <hbt@markhamboards.com>
2018-02-08 00:50:03 -0500

    Edit App Mods

--- mdbs/src/app/app.module.ts
+++ mdbs/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
